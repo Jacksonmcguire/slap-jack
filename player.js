b14456a1601e9b3c9c56da707a1dab5fc6912fe5
Jackson McGuire <jaxmcguire@gmail.com>
2021-01-14 16:24:25 -0500

    Start building playCard and saveWins

--- player.js
+++ player.js
@@ -1,13 +1,16 @@
 class Player {
   constructor() {
-    this.id;
+    this.id = Date.now();
     this.wins;
-    this.hand
+    this.hand = [];
   }
-  playCard() {
-
+  playCard(game) {
+    if(this.hand.length > 0) {
+      var activeCard = this.hand.shift();
+      game.deck.push(activeCard);
+    }
   }
   saveWinsToStorage() {
-
+    localStorage.setItem(`${this.id}`, this.wins);
   }
 }
