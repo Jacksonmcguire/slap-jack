aaec4340ee0cc13c82b291d0e24a22d61f2f3352
Jackson McGuire <jaxmcguire@gmail.com>
2021-01-14 16:06:26 -0500

    Create random element, and createCardVariables functions

---
+++ main.js
@@ -0,0 +1,30 @@
+// Query Selectors
+
+// Event Listeners
+
+// Functions
+function getRandomElement(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
+
+function createCardVariables(color) {
+  var currentColor = color;
+  var deck = [];
+  var currentCard = '';
+  for(var i = 0; i < 13; i ++) {
+    if(i < 10) {
+      currentCard = color + (i + 1);
+      deck.push(currentCard);
+    } else if(i === 10) {
+      currentCard = color + 'Jack';
+      deck.push(currentCard);
+    } else if(i === 11) {
+      currentCard = color + 'Queen';
+      deck.push(currentCard);
+    } else if(i === 12) {
+      currentCard = color + 'King';
+      deck.push(currentCard);
+    }
+  }
+  return deck;
+}
