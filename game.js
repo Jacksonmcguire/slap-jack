16800d52e61b31e4a6a9be1d3d280c45d224af54
Jackson McGuire <jaxmcguire@gmail.com>
2021-01-14 18:55:16 -0500

    Build game methods

--- game.js
+++ game.js
@@ -3,15 +3,58 @@ class Game {
     this.player1 = new Player();
     this.player2 = new Player();
     this.deck = (createCardVariables('blue') + `,${createCardVariables('gold')}` + `,${createCardVariables('green')}` + `,${createCardVariables('red')}`).split(',');
-    this.currentPlayer;
+    this.currentPlayer = this.player1;
   }
-  shuffle(deck) {
-    var shuffleDeck = [];
+  shuffle() {
     for(var i = 0; i < this.deck.length; i++) {
-      var beingShuffled = getRandomElement(this.deck);
-      shuffleDeck.push(beingShuffled);
+      var randomInt = Math.floor(Math.random() * this.deck.length);
+      var temporaryInt = this.deck[i];
+      this.deck[i] = this.deck[randomInt];
+      this.deck[randomInt] = temporaryInt;
     }
-    this
-    this.deck = shuffleDeck;
+  }
+
+  trackCentralDeck() {
+    return this.deck[0];
+  }
+
+  dealToMiddle() {
+    this.currentPlayer.playCard(this);
+    if(this.currentPlayer === this.player1) {
+      this.currentPlayer = this.player2;
+    } else {
+      this.currentPlayer = this.player1;
+    }
+  }
+
+  dealHands() {
+    this.shuffle(this.deck)
+    for(var i = 0; i < this.deck.length; i++) {
+      if(i % 2 === 0) {
+        this.player1.hand.push(this.deck[i]);
+      } else {
+        this.player2.hand.push(this.deck[i]);
+      }
+    }
+    this.deck = [];
+  }
+
+  slap(player) {
+    if(this.deck[0].includes('Jack')) {
+      player.hand = player.hand.concat(this.deck);
+      this.deck = [];
+      console.log('SlapJack');
+    } else if(this.deck[0].slice(-1) === this.deck[1].slice(-1)) {
+      player.hand = player.hand.concat(this.deck);
+      this.deck = [];
+      console.log('Doubles!');
+    } else if(this.deck[0].slice(-1) === this.deck[2].slice(-1)) {
+      player.hand = player.hand.concat(this.deck);
+      this.deck = [];
+      console.log('Sandwich');
+    }
+  }
+  win(player) {
+    player.wins ++;
   }
 }
